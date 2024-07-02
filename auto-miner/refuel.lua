e84b707837610d18a7c6458144ffcb31e2377916
LuunaVR <101238744+LuunaVR@users.noreply.github.com>
2024-07-02 04:37:57 -0700

    Update refuel.lua

--- auto-miner/refuel.lua
+++ auto-miner/refuel.lua
@@ -1,8 +1,27 @@
-print("Enter amount (in buckets): ")
-local amount = read()
-for i = 1, amount do
-  turtle.drop()
-  turtle.suck()
-  turtle.refuel()
-  print(turtle.getFuelLevel())
+local amount
+ 
+function init()
+  print("Enter amount or hit enter for max")
+  amount = read()
+  if amount == "" then
+    amount = math.huge
+  elseif tonumber(amount) < 0 then
+    error("Negative numbers not allowed")
+  end
 end
+ 
+function main()
+  init()
+  for i = 1, amount do
+    if turtle.getFuelLevel() == turtle.getFuelLimit() then
+      print("Fuel reached limit")
+      return
+    end
+    turtle.drop()
+    turtle.suck()
+    turtle.refuel()
+    print("Fuel: " .. turtle.getFuelLevel())
+  end  
+end
+ 
+main()
