196c7ae8c19f61b29a7ddee6e355640bf1ba317e
Luuna <101238744+LuunaVR@users.noreply.github.com>
2025-05-08 20:27:44 -0700

    Update message.lua

--- misc/message.lua
+++ misc/message.lua
@@ -3,7 +3,7 @@ local chatBox = peripheral.find("chatBox")  -- Automatically finds the Chat Box 
 -- Player list to send to when sending to all
 local players = {"foeslayerx","lunabear01","luunavr","mystic271","perolith","tantebouster"}
 
-local rickroll = "YOU HAVE BEEN RICK ROLLED!! We're no strangers to love...You know the rules and so do I...A full commitment's what I'm thinking of...."
+local rickroll = "YOU HAVE BEEN RICK ROLLED!! We're no strangers to love...You know the rules and so do I..."
 
 -- Confirm to send
 term.write("Send prank message? Y/N: ")
